11d7d284e268d9b415ea4dc71ea123f89290028e
janghwankim <hwan525@naver.com>
2023-07-21 15:31:05 +0900

    a4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
-import ScrollBox from './ScrollBox';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
+//램덤 색상을 생성합니다
+function getRandomColor(){
+  return '#'+Math.floor(Math.random() * 1677215).toString(16);
+}
 
 class App extends Component {
-  render(){
-    return(
-      <div>
-        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-        <button onClick={() => this.ScrollBox.ScrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+  state = {
+    color: '#000000'
   }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+render() {
+  return (
+    <div>
+      <button onClick={this.handleClick}>랜덤 색상</button>
+      <ErrorBoundary>
+      <LifeCycleSample color= {this.state.color} />
+      </ErrorBoundary>
+    </div>
+  );
+}
 }
 
 export default App;
